edd6e523357d20a126bff20dccf82b45e7fa541e
Brandon Kamaka <Bkamaka@gmail.com>
2018-07-03 13:38:11 -0400

    fist commit

---
+++ cmake-build-debug/CMakeFiles/Fork2.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Fork2.dir/main.c.obj"
+  "Fork2.pdb"
+  "Fork2.exe"
+  "Fork2.exe.manifest"
+  "libFork2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Fork2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+int main() {
+    printf("Hello, World!\n");
+
+    char *testChar;
+    pid_t pid;
+
+    fork();
+    
+    
+
+    return 0;
+}
